abe27685ac2d3e0bee0c2e60febf7f9347d1cd01
sebas <sebastian.londono75@correo.tdea.edu.co>
2025-03-21 17:00:42 -0500

    FIX problema del cors para prueba

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
-const {PORT, SERVER} = process.env;
+const { PORT, SERVER } = process.env;
 
 const app = express();
 
-console.log("ahhhhhh")
+console.log("ahhhhhh");
+
 app.use(cors({
-  origin: [`${SERVER}`],
-  methods: 'GET,POST,PUT,DELETE',
+  origin: SERVER ? [SERVER] : '*',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
   credentials: true
 }));
 
-app.get('/niah', (req, res) => res.send('hello world'))
-app.listen(PORT, ()=> {
-  console.log(`Server running on port: ${PORT}`)
-})
+app.options('*', (req, res) => {
+  res.header("Access-Control-Allow-Origin", SERVER || '*');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.sendStatus(200);
+});
+
+app.get('/niah', (req, res) => res.send('hello world'));
+
+app.listen(PORT || 3000, () => {
+  console.log(`Server running on port: ${PORT || 3000}`);
+});
