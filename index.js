ad9d268c99ca528cc19293b73a6a4ce4bb54543b
sebas <sebastian.londono75@correo.tdea.edu.co>
2025-03-21 16:46:19 -0500

    ADD cors

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
 
-const {PORT} = process.env;
+const {PORT, SERVER} = process.env;
 
 const app = express();
 
 console.log("ahhhhhh")
+app.use(cors({
+  origin: [`${SERVER}`],
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
 
 app.get('/niah', (req, res) => res.send('hello world'))
 app.listen(PORT, ()=> {
